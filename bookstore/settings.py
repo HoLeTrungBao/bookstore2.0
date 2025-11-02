5c0469976749b49d9ab5aa55611bbc37e63e6eb2
HoLeTrungBao <baoho757@gmail.com>
2025-11-02 13:29:37 +0700

    Fix allowed hosts for Render

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-bookstore-secret-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bookstore2-0.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
